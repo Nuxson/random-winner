65ba5caa1a4119136ec6172642e18c8bc6c9a4d7
Nuxson <nik-10-1000@yandex.ru>
2022-07-29 19:49:42 +0500

    commit

---
+++ data.js
@@ -0,0 +1,62 @@
+const users = [
+    {
+        id: 1,
+        name:'Вася',
+        nickname:'vasy',
+    },
+    {
+        id: 2,
+        name:'Ник',
+        nickname:'nitak',
+    },
+    {
+        id: 3,
+        name:'Коля',
+        nickname:'Kolynys',
+    },
+    {
+        id: 4,
+        name:'Валера',
+        nickname:'Valersan',
+    },
+    {
+        id: 5,
+        name:'Кристина',
+        nickname:'Krest',
+    },
+    {
+        id: 6,
+        name:'Арина',
+        nickname:'Armyanina',
+    },
+    {
+        id: 7,
+        name:'Илья',
+        nickname:'Bkmz',
+    },
+    {
+        id: 8,
+        name:'Никита',
+        nickname:'Nikitosick',
+    },
+    {
+        id: 9,
+        name:'Юра',
+        nickname:'Yr4ick',
+    },
+    {
+        id: 10,
+        name:'Гамза',
+        nickname:'Gazon',
+    },
+    {
+        id: 11,
+        name:'Дарья',
+        nickname:'Soroka',
+    },
+    {
+        id: 12,
+        name:'Светлана',
+        nickname:'Unknown Cloud',
+    },
+]
---
+++ index.js
@@ -0,0 +1,63 @@
+const dom ={
+    remained: document.getElementById('remained'),
+    result: document.getElementById('result'),
+    winner: {
+        name: document.getElementById('winner-name'),
+        nickname: document.getElementById('winner-nickname'),
+    },
+    btn: document.getElementById('btn')
+}
+
+let filteredUsers = users
+renderRemainedUser(filteredUsers.length)
+
+function renderRemainedUser(count){
+    dom.remained.innerHTML = count
+}
+
+//Клик по кнопке
+dom.btn.onclick = () =>{
+    const winner = getRandomUser(filteredUsers);
+
+    if(winner){
+        filteredUsers = getFilteredUser(filteredUsers, winner)
+        renderWinner(winner)
+    }
+    else{
+        dom.btn.style.display = 'none'
+        alert('Участники закончились')
+
+    }
+    renderRemainedUser(filteredUsers.length)
+    filteredUsers = getFilteredUser(filteredUsers, winner);
+    renderWinner(winner);
+    
+}
+//Функция выбора случайного пользователя
+function getRandomUser(users) {
+    const maxIdx = users.length - 1
+    const winnerIdx = Math.floor(Math.random() * maxIdx)
+    return users[winnerIdx]
+}
+
+
+//Отрисовка блока победителя
+function renderWinner(winner){
+
+    dom.result.style.display = 'block'
+    dom.winner.name.innerHTML = winner.name
+    dom.winner.nickname.innerHTML = winner.nickname
+}
+//Фильтрация массива
+function getFilteredUser(users, winner){
+    const filterUsers = users.filter(user => user.id !== winner.id)
+    return filterUsers
+}
+
+function renderAnimatedRandomUsers(filteredUsers){
+    //Интервал
+    setInterval(() => {
+        const randomUser = getFilteredUser(filteredUsers)
+        renderWinner(randomUser)
+    }, 1000)
+}
